f430e474b967ded88383f8e0c320877f0317e7e8
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-01-05 19:00:14 +0200

    check user

--- controllers/establishment.js
+++ controllers/establishment.js
@@ -5,6 +5,7 @@ exports.connect = async (req, res, next) => {
     //create a new user
     const phone = req.body.phone;
     const public_key = req.body.public_key;
+    console.log("public key: ",public_key);
    
     //create a new user
     const user = new User({
@@ -32,23 +33,32 @@ exports.connect = async (req, res, next) => {
 //check if the user is in the database
 exports.check = async (req, res, next) => {
     //post request with the body of the request containing phone number
-    //create a new user
     const phone = req.body.phone;
    
-    //find user with phone number
     try{
-    //save the user
-        result = await user.save();
-        //get all public keys from users but the current user
-        const users = await User.find({_id: {$ne: result._id}});
-        //send all public keys to the user
-        res.status(201).json({
-            message: 'User created',
-            users: users.map(user => {
-                return {public_key: user.public_key};
-            })
-        });
-
+    //find user with phone number
+        let user = await User.findOne({phone: phone});
+        //if user exists
+        if(user)
+        {
+                if(user){
+                    //if user exists
+                    res.status(201).json({
+                        message: 'User exists',
+                        public_key: user.public_key
+                    });
+                }else{
+                    //if user does not exist
+                    res.status(201).json({
+                        message: 'User does not exist'
+                    });
+                }
+        }else{
+            //user doesnt exist
+            res.status(404).json({
+                message: 'User does not exist'
+            });
+        }
     }catch(err){
         console.log(err);
     }
--- routes/establishment.js
+++ routes/establishment.js
@@ -8,6 +8,7 @@ const router = express.Router();
 const establishmentController = require('../controllers/establishment');
 
 
-router.post('/connect', establishmentController.connect);
 
+router.post('/connect', establishmentController.connect);
+router.post('/check', establishmentController.check);
 module.exports = router;
